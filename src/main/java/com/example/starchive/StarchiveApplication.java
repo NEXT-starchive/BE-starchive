7a20f7f128376b0736e67433cab42c35f387562c
newxxson <hyukjun1111@gmail.com>
2023-11-22 22:11:06 +0900

    completed unnoticed merge leftover

--- src/main/java/com/example/starchive/StarchiveApplication.java
+++ src/main/java/com/example/starchive/StarchiveApplication.java
@@ -3,14 +3,9 @@ package com.example.starchive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Value;
 import javax.annotation.PostConstruct;
-=======
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
->>>>>>> 029a5ea49e56ecb73ab6e01d318d4cb08160586f
-
-
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -18,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class StarchiveApplication {
   @Value("${CHROME_DRIVER_PATH}")
   private String chromeDriverPath;
+
   public static void main(String[] args) {
     SpringApplication.run(StarchiveApplication.class, args);
   }
